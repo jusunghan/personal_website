d9a84253a34a53a6fd1e4685a6f4964d1b9744cb
Jusung Han <jasicat37@gmail.com>
2018-07-18 15:29:36 -0500

    added animation on resume page

---
+++ js/resume.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.page-scroll').on('click', function(){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+})
